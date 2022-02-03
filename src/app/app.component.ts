d3e4aa73c60861e5a0f6211f37fc41d3f130e8c6
cindyjong <viddrawings@gmail.com>
2022-02-03 23:29:04 +0100

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,116 +1,61 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public title = 'Scuttles';
 
   public data: any;
+
   public form: FormGroup;
-  public formBuilder: any;
+  public submitted = false;
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      newGameName: [''],
-      game: this.fb.group({
-        playerName: ['']
-      })
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      gameName: ['', Validators.required],
+      playersBlueTeam: new FormArray([]),
+      playersRedTeam: new FormArray([])
     });
   }
 
-  public ngOnInit(): void {
+  get formControls() { return this.form.controls; }
+  get playersBlueTeamFormControls() { return this.formControls['playersBlueTeam'] as FormArray; }
+  get playersBlueTeam() { return this.playersBlueTeamFormControls.controls as FormGroup[]; }
+
+  get playersRedTeamFormControls() { return this.formControls['playersRedTeam'] as FormArray; }
+  get playersRedTeam() { return this.playersRedTeamFormControls.controls as FormGroup[]; }
+
+  public onGameName(event: any): void {
+    const gamesName = event.target.value;
+
+    for (let i = this.playersBlueTeamFormControls.length; i < 5; i++) {
+      this.playersBlueTeamFormControls.push(this.formBuilder.group({
+        playerNameBlueTeam: [''],
+        playerGivePointsBlueTeam: [''],
+      }));
+    }
+
+    for (let i = this.playersRedTeamFormControls.length; i < 5; i++) {
+      this.playersRedTeamFormControls.push(this.formBuilder.group({
+        playerNameRedTeam: [''],
+        playerGivePointsRedTeam: [''],
+      }));
+    }
   }
 
-  public addGame(data: any): void {
-    this.data = {
-      games: [],
-      newGameName: data.target.value
-    };
-    this.data.games.push({
-      name: data.target.value,
-      players: [],
-      newPlayerName: '',
-      betLog: [],
-    });
+  public onSubmit(): void {
+    this.submitted = true;
 
-    console.log(this.data.games);
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    // display form values on success
+    // alert(JSON.stringify(this.form.value, null, 4));
   }
-
-  public addPlayer(game: any): void {
-    console.log(game);
-    game.players.push({
-        name: game.playerName,
-        points: 1000,
-        bet: {
-          points: 0,
-          win: null
-        }
-      }
-    );
-    game.playerName = '';
-  }
-
-  // public givePointsToPlayer(player: any, points: any): void {
-  //   player.points = points;
-  // }
-  //
-  // public bet(player: any, win: any): void {
-  //   player.points -= 1;
-  //   player.bet.points += 1;
-  //   player.bet.win = win
-  // }
-  //
-  // public cancelBet(player: any): void {
-  //   player.points += player.bet.points;
-  //   player.bet.points = 0;
-  //   player.bet.win = null;
-  // }
-  //
-  // public payOut(game: any, win: any): void {
-  //   let lostBetting = 0;
-  //   let wonBetting = 0;
-  //   let log = '';
-  //   for (let player of game.players) {
-  //     if (player.bet.win !== win) {
-  //       lostBetting += player.bet.points;
-  //       log += player.name + ' lost ' + player.bet.points + ';';
-  //     } else {
-  //       wonBetting += player.bet.points;
-  //       player.points += player.bet.points;
-  //     }
-  //   }
-  //   for (let player of game.players) {
-  //     if (player.bet.win === win) {
-  //       let share = player.bet.points / wonBetting;
-  //       player.points += share * lostBetting;
-  //       log += player.name + ' won ' + (share * lostBetting) + ';';
-  //     }
-  //     player.bet.points = 0;
-  //     player.bet.win = null;
-  //   }
-  //   game.betLog.push(log)
-  // }
-  //
-  // public saveToLocalStorage(games: any): void {
-  //   localStorage.setItem('edenBettingBackup', JSON.stringify(games));
-  // }
-  //
-  // public loadFromLocalStorage(games: any): void {
-  //   // let state = JSON.parse(localStorage.getItem('edenBettingBackup'));
-  //   // for (let game of state) {
-  //   //   games.push(game);
-  //   // }
-  // }
-  //
-  // public formatPoints(number: any): string {
-  //   let formatter = new Intl.NumberFormat(`en-US`, {
-  //     currency: `BTC`,
-  //     style: 'currency',
-  //   });
-  //   return formatter.format(number);
-  // }
 }
